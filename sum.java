5792b181a5880dc155aa242000e7014819a6b1ce
Krish Agrawal <krishagrawal3914@gmail.com>
2023-04-15 01:35:21 +0530

    1st commit

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String[] args){
+        System.out.println("hello world");
+         int a=2;
+         int b=4;
+         System.out.println(a+b);
+
+     }
+}
