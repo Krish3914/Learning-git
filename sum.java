0c9e4783daf5e6c5027ad19282e7f5e022e719b8
Krish Agrawal <krishagrawal3914@gmail.com>
2023-04-15 12:19:42 +0530

    dev cache

--- sum.java
+++
@@ -1,9 +0,0 @@
-public class sum {
-    public static void main(String[] args){
-        System.out.println("hello world");
-         int a=2;
-         int b=4;
-         System.out.println("the sum is "+ (a+b));
-
-     }
-}
